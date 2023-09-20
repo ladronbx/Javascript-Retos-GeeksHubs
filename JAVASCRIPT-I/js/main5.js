9e21fdd093af9b5d59a055ebb590f8b51b73b400
ladronbx <ladronbravovlc@gmail.com>
2023-09-20 16:11:21 +0200

    Reto 5 completado

--- JAVASCRIPT-I/js/main5.js
+++ JAVASCRIPT-I/js/main5.js
@@ -1,2 +1,11 @@
 // 5. Lee un número por teclado e indica si es divisible entre 2 (resto = 0). Si no lo es, también
 // debemos indicarlo.
+
+let number = parseInt(prompt("Inserta un número:"));
+
+
+if(number % 2 === 0){
+    alert('El resto es 0 por lo que sí es divisible entre 2')
+}else{
+    alert('No es divisible entre 2')
+}
