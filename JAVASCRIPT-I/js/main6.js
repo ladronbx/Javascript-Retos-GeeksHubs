dc0e21034091357c71b42637178cfe1d11cffa10
ladronbx <ladronbravovlc@gmail.com>
2023-09-20 16:17:39 +0200

    Reto 6 completado

--- JAVASCRIPT-I/js/main6.js
+++ JAVASCRIPT-I/js/main6.js
@@ -1,2 +1,12 @@
-// 6. Lee un número por teclado que pida el precio de un producto (puede tener decimales) y
-// calcule el precio final con IVA. El IVA será una constante que sera del 21%
+// 6. Lee un número por teclado que pida el precio de
+//  un producto (puede tener decimales) y
+// calcule el precio final con IVA. 
+// El IVA será una constante que sera del 21%
+
+let precio = parseFloat(prompt("Inserta el precio sin iva:"));
+
+const iva = 21;
+
+let resultado = ((precio*21) / 100) + precio;
+
+alert(`El resultado es ${resultado}`);
