bd48261d241f24ea36701a8a91ffeff525672341
ladronbx <ladronbravovlc@gmail.com>
2023-09-20 16:58:47 +0200

    Reto 11 completado

--- JAVASCRIPT-I/js/main11.js
+++ JAVASCRIPT-I/js/main11.js
@@ -1,2 +1,23 @@
 // 11. Crea una aplicación que nos pida un día de la semana y que nos diga si es un día laboral
 // o no. Usa un switch para ello.
+
+
+
+let dia = prompt('Dime un día de la semana: ').toLowerCase();
+
+switch(dia){
+    case "lunes":
+    case "martes":
+    case "miércoles":
+    case "jueves":
+    case "viernes":
+        console.log('Entre semana');
+        break;
+    case "sábado":
+    case "domingo":
+        console.log('Findeeeeee');
+        break;
+    default:
+        console.log('No es un día válido');
+        break;
+};
