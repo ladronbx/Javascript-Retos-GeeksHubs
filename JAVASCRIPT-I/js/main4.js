7897c5b3d2be4cffddfc479e6b2baa9f16482b21
ladronbx <ladronbravovlc@gmail.com>
2023-09-20 15:45:24 +0200

    Reto 4 completado

--- JAVASCRIPT-I/js/main4.js
+++ JAVASCRIPT-I/js/main4.js
@@ -1,3 +1,26 @@
 
 // 4. Haz una aplicación que calcule el área de un círculo (pi*R2). El radio se pedirá por teclado
 // (recuerda pasar de String a double con parseDouble). Usa la constante PI.
+
+
+//"parseDouble"???? Es Java. No JavaScript. Usaré pues "parseFloat"
+
+let radio = parseFloat(prompt("Inserta el radio del círculo:"));
+
+
+if (isNaN(radio)) {
+
+    alert("El valor ingresado no es válido. Por favor, ingresa un número válido.");
+
+
+} else {
+
+    let area = Math.PI * Math.pow(radio, 2);
+
+    alert(`El área del círculo con radio ${radio} es: ${area.toFixed(2)}`);
+
+}
+
+
+
+//Math.pow ->  Se utiliza para elevar un número a una potencia específica (radio * radio)
