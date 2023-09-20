ec8feff5e78a7b4a76ebde1d53caa2d7465fc5e1
ladronbx <ladronbravovlc@gmail.com>
2023-09-20 15:30:49 +0200

    Reto 2 completado

--- JAVASCRIPT-I/js/main2.js
+++ JAVASCRIPT-I/js/main2.js
@@ -1,2 +1,6 @@
 // 2. Declara un String que contenga tu nombre, después muestra un mensaje de bienvenida
 // por consola. Por ejemplo: si introduzco “Fernando”, me aparezca “Bienvenido Fernando”.
+
+let name = 'Bienvenida';
+
+console.log(`Bienvenida ${name} (tipical xd)`)
