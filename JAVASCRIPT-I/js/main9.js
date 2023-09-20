6c134c63e37ce9a2bf3b5f1cd6690d0c06ce57b9
ladronbx <ladronbravovlc@gmail.com>
2023-09-20 16:26:42 +0200

    Reto 9 completado

--- JAVASCRIPT-I/js/main9.js
+++ JAVASCRIPT-I/js/main9.js
@@ -1,2 +1,8 @@
 // 9. Muestra los números del 1 al 100 (ambos incluidos) divisibles entre 2 y 3. Utiliza el bucle
 // que desees.
+
+for(let i = 0; i <= 100; i++){
+    if((i%2 === 0) && (i%3 === 0)){
+        console.log(i);
+    }
+}
