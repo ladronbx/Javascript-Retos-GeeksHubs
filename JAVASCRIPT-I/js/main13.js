2b97c7a42b677c444829ab0cc98e256fc53a44b0
ladronbx <ladronbravovlc@gmail.com>
2023-09-21 10:08:52 +0200

    Reto 13 : Duda en exponente y módulo

--- JAVASCRIPT-I/js/main13.js
+++ JAVASCRIPT-I/js/main13.js
@@ -6,5 +6,71 @@
 // -: resta los operandos.
 // *: multiplica los operandos.
 // /: divide los operandos, este debe dar un resultado con decimales (double)
+
+
+
+
 // ^: 1o operando como base y 2o como exponente.
 // %: módulo, resto de la división entre operando1 y operando2.
+
+
+
+let x = parseInt(prompt("Inserta el primer número"));
+let y = parseInt(prompt("Inserta el segundo número"));
+
+
+
+let signo = prompt("Inserta un signo para operar");
+
+let resultado;
+
+function suma(x, y){
+    resultado = x + y;
+    return resultado;
+}
+
+function resta(x, y){
+    resultado = x - y;
+    return resultado;
+}
+
+function multiplica(x, y){
+    resultado = x * y;
+    return resultado;
+}
+
+function divide(x, y){
+    resultado = x / y;
+    return resultado;
+}
+
+function exponente(x, y){
+    resultado = Math.exp(x, y);
+    return resultado;
+}
+
+function modulo(x, y){
+    resultado = x%y;
+    return resultado;
+}
+
+switch(signo){
+    case "+":
+        alert(suma(x, y));
+    break;
+    case "-":
+        alert(resta(x, y));
+    break;
+    case "*":
+        alert(multiplica(x, y));
+    break;
+    case "/":
+        alert(divide(x, y));
+    break;
+    case "^":
+        alert(exponente(x, y));
+    break;
+    case "%":
+        alert(modulo(x, y));
+    break;
+}
