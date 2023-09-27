6b456eb1bc39386743b4cb4130ce5252212e173b
ladronbx <ladronbravovlc@gmail.com>
2023-09-27 19:14:42 +0200

    JavaScript III. Exercise 6 completed

--- JAVASCRIPT-III/condicionales/main6.js
+++ JAVASCRIPT-III/condicionales/main6.js
@@ -1,1 +1,15 @@
 // 6. Pedir una cadena de texto y comprobar que todas sus letras estén en minúsculas.
+
+let text = prompt('Dime una cadena de texto:');
+
+const check = (text) => {
+    const textoEnMinusculas = text.toLowerCase();
+    
+    if (text === textoEnMinusculas) {
+        console.log('Todas las letras están en minúsculas.');
+    } else {
+        console.log('No todas las letras están en minúsculas.');
+    }
+}
+
+check(text);
