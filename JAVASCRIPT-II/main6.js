7e845ab6681b262e89cc463e635ad1256d3e4a34
ladronbx <ladronbravovlc@gmail.com>
2023-09-27 18:07:58 +0200

    JavaScript II. Exercise 5 completed

--- JAVASCRIPT-II/main6.js
+++ JAVASCRIPT-II/main6.js
@@ -1,3 +1,17 @@
 // 6. Crea una aplicación que nos cuente el número de cifras de un número entero positivo
 // (hay que controlarlo) pedido por prompt. Crea un método que realice esta acción, pasando
 // el número por parámetro devolverá el número de cifras.
+
+// Flujo:
+// 1. Debe contar el número de cifras de un número entero positivo
+
+
+
+let x = 12345;
+let number;
+
+const calculator = (x) => {
+    let string = x.toString();
+    return console.log(string.length);
+};
+calculator(x);
--- JAVASCRIPT-II/main7.js
+++ JAVASCRIPT-II/main7.js
@@ -3,7 +3,34 @@
 // parámetros, la cantidad de euros y la moneda a pasar que será una cadena, este no
 // devolverá ningún valor, mostrará un mensaje indicando el cambio.
 
-// El cambio de divisas es:
-// • 0.86 libras es un 1 €
-// • 1.28611 $ es un 1 €
-// • 129.852 yenes es un 1 €
+// La tasa de cambio :
+// • 0.86L es un 1 €
+// • 1.28611$ es un 1 €
+// • 129.852Y es un 1 €
+
+
+let cantidad; // Esto lo indicará el usuario
+let moneda;  // Esto lo indicará el usuario
+
+const libra = 'libra';  // Cambiamos a cadenas
+const dolar = 'dolar';  // Cambiamos a cadenas
+const yen = 'yen';      // Cambiamos a cadenas
+let result;
+
+const calculator = (cantidad, moneda) => {
+    if (moneda === libra) {
+        result = cantidad * 0.86;
+        return console.log(result);
+    } else if (moneda === yen) {
+        result = cantidad * 129.852;
+        return console.log(result);
+    } else if (moneda === dolar) {
+        result = cantidad * 1.28611;
+        return console.log(result);
+    } else {
+        console.log('No calculo esa moneda');
+    }
+}
+
+calculator(2, 'libra');
+
